28b2435c62a1de7299369bebc2ff1e1c6a857113
Stefan Mächler <stefan.maechler@gmail.com>
2018-09-21 09:18:55 +0200

    Improve jsonparser example with key argument

--- example/jsonparser.c
+++ example/jsonparser.c
@@ -5,13 +5,16 @@
 
 static void print_usage(void)
 {
-    fprint(stderr, "usage: jsonparser FILE\n");
+    fprint(stderr, "usage: jsonparser FILE [KEY]\n");
 }
 
 int main(int argc, char *argv[])
 {
     int ret = 0;
-    if (argc != 2) {
+    char *key = NULL;
+    if (argc == 3) {
+        key = argv[2];
+    } else if (argc < 2 || argc > 3) {
         print_usage();
         return -1;
     }
@@ -20,9 +23,19 @@ int main(int argc, char *argv[])
         Str *js_str = file_read(f, -1);
         Json *js = json_new_cstr(str_cstr(js_str));
         if (json_is_valid(js)) {
-            json_pretty_print(js);
+            if (key != NULL) {
+                void *node = json_get_node(js, key);
+                if (node != NULL) {
+                    put(node);
+                } else {
+                    fprint(stderr, "Error: No such key '%s'!\n", key);
+                }
+            } else {
+                json_pretty_print(js);
+            }
         } else {
             fprint(stderr, "Error: %s!\n", json_err_msg(js));
+            ret = -1;
         }
         delete(js);
         delete(js_str);       
