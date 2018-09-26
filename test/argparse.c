83b6e52008af406fe8d1d8f0d070ef568d85e2c7
Stefan Mächler <stefan.maechler@neratec.com>
2018-09-26 14:58:18 +0200

    Improve argparse test program for manual tests

--- test/argparse.c
+++ test/argparse.c
@@ -13,18 +13,20 @@ int main(int argc, char *argv[])
 {
     Argparse *ap = new(Argparse, path_basename(argv[0]),
             "This is the help text of the argparse test program.");
-    put(ap);
     argparse_add_opt(ap, 't', NULL, NULL, NULL, NULL, "help for flag t");
     argparse_add_opt(ap, 'f', "foo", NULL, "2", argparse_num, "help for foo");
     argparse_add_arg(ap, "cmd", "CMD", NULL, cmd_check, "help test for num");
     argparse_add_arg(ap, "file", "FILE", "+", argparse_file, NULL);
-    argparse_print_help(ap);
     Map *args;
-    if(argc > 1) {
-        args = argparse_parse(ap, argc, argv);
-    } else {
+    if(argc == 1) {
+        // Test program
+        put(ap);
+        argparse_print_help(ap);
         char *test_args[] = {"", "-f", "12", "0.5", "set", "-t", "/etc/passwd"};
         args = argparse_parse(ap, ARRAY_LEN(test_args), test_args);
+    } else {
+        // Manual tests
+        args = argparse_parse(ap, argc, argv);
     }
     put(args);
     delete(args);
