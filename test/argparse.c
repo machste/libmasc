02e2db4dbe4065fe6cb29fe12bddbf7e5e065342
Stefan Mächler <stefan.maechler@gmail.com>
2018-09-27 09:21:09 +0200

    Improve argparse test for type check functions

--- test/argparse.c
+++ test/argparse.c
@@ -1,12 +1,20 @@
 #include <masc/argparse.h>
 #include <masc/path.h>
+#include <masc/cstr.h>
 #include <masc/macro.h>
 #include <masc/print.h>
 
 
 void *cmd_check(Str *cmd, Str **err_msg)
 {
-    return new_copy(cmd);
+    static char *valid_cmds[] = {"get", "set"};
+    for (int i = 0; i < ARRAY_LEN(valid_cmds); i++) {
+        if (cstr_eq(str_cstr(cmd), valid_cmds[i])) {
+            return new_copy(cmd);
+        }
+    }
+    *err_msg = str_new("invalid command: '%O'!", cmd);
+    return NULL;
 }
 
 int main(int argc, char *argv[])
@@ -15,7 +23,7 @@ int main(int argc, char *argv[])
             "This is the help text of the argparse test program.");
     argparse_add_opt(ap, 't', NULL, NULL, NULL, NULL, "help for flag t");
     argparse_add_opt(ap, 'f', "foo", NULL, "2", argparse_num, "help for foo");
-    argparse_add_arg(ap, "cmd", "CMD", NULL, cmd_check, "help test for num");
+    argparse_add_arg(ap, "cmd", "CMD", NULL, cmd_check, "command: get or set");
     argparse_add_arg(ap, "file", "FILE", "+", argparse_file, NULL);
     Map *args;
     if(argc == 1) {
