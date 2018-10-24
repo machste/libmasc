9a175579a0bbba3d2226a0a0ea1a563c701f0c42
Stefan Mächler <stefan.maechler@gmail.com>
2018-10-24 22:24:28 +0200

    Split iterable and iterable_class to separate files

--- include/masc/iterable.h
+++ include/masc/iterable.h
@@ -1,8 +1,7 @@
 #ifndef _MASC_ITERABLE_H_
 #define _MASC_ITERABLE_H_
 
-#include <stdbool.h>
-
+#include <masc/iterable_class.h>
 #include <masc/object.h>
 #include <masc/iter.h>
 
@@ -11,30 +10,8 @@ typedef struct Iterable {
     Object;
 } Iterable;
 
-typedef void *(*new_priv_cb)(Iterable *self);
-typedef void *(*next_cb)(Iterable *self, void *priv);
-typedef void (*del_obj_cb)(Iterable *self, void *priv);
-typedef bool (*is_last_cb)(Iterable *self, void *priv);
-typedef int (*get_idx_cb)(Iterable *self, void *priv);
-typedef const char *(*get_key_cb)(Iterable *self, void *priv);
-typedef void (*delete_priv_cb)(void *priv);
 
-typedef struct {
-    class;
-    new_priv_cb new_priv;
-    next_cb next;
-    del_obj_cb del_obj;
-    is_last_cb is_last;
-    get_idx_cb get_idx;
-    get_key_cb get_key;
-    delete_priv_cb delete_priv;
-} iterable_class;
-
-typedef bool (*filter_cb)(Object *obj);
-
-const iterable_class *iterable_class_of(const Iterable *self);
-
-void filter(Iterable *iterable, filter_cb cb);
+Iter *iterable_iter(Iterable *self);
 
 
-#endif /* _MASC_ITER_H_ */
+#endif /* _MASC_ITERABLE_H_ */
--- src/iterable.c
+++ src/iterable.c
@@ -1,22 +1,7 @@
 #include <masc/iterable.h>
 
 
-const iterable_class *iterable_class_of(const Iterable *self)
+Iter *iterable_iter(Iterable *self)
 {
-    if (self != NULL) {
-        return (iterable_class *)self->cls;
-    } else {
-        return NULL;
-    }
-}
-
-void filter(Iterable *iterable, filter_cb cb)
-{
-    Iter i = init(Iter, iterable);
-    for (void *obj = next(&i); obj != NULL; obj = next(&i)) {
-        if (!cb(obj)) {
-            iter_del_obj(&i);
-        }
-    }
-    destroy(&i);
+    return new(Iter, self);
 }
