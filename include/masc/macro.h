87ee0e76893228620a635654b3f3ae72843eae80
Stefan Mächler <stefan.maechler@westermo.com>
2022-01-21 10:07:56 +0100

    Add macro to convert macro content to c-string

--- include/masc/macro.h
+++ include/masc/macro.h
@@ -1,6 +1,9 @@
 #ifndef _MASC_MACRO_H_
 #define _MASC_MACRO_H_
 
+#define QUOTE(name) #name
+#define CSTR(macro) QUOTE(macro)
+
 #define ARRAY_LEN(_array) (sizeof(_array) / sizeof(_array)[0])
 
 #define LAMBDA(ret_type, fn_body) ({ret_type __fn__ fn_body; __fn__;})
