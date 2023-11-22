4497585e30f03747365d15d0dd47553645ae2a78
Stefan Mächler <stefan.maechler@neratec.com>
2023-11-22 11:52:21 +0100

    Test UTF-8 characters in JSON module

--- test/json.c
+++ test/json.c
@@ -10,12 +10,12 @@ int main(int argc, char *argv[])
     Json *js = json_new_cstr("[1, 3.12, \"string\", {\"1\": 1, \"2\": false}]");
     put_repr(js);
     put(js);
-    json_parse(js, "{\"node\":{\"a\":[[null, -0.1, {\"foo\":\"bar\", \"a.b\": 666}], 42]}}");
+    json_parse(js, "{\"node\":{\"a\":[[null, -0.1, {\"foo\":\"bar\", \"a.b\": 666}], 42], \"utf-8\":\"\\xc2\\xa2\"}}");
     json_pretty_print(js);
     // Get node
     char *get_keys[] = {"", "node.a[0][0]", "node", "[\"node\"][\"a\"][1]",
         "a[b.c]", "node.a[0][1]", "node.a[0][2].foo", "node.a[0][2][\"a.b\"]",
-        "gugus", "[\"node\"]"
+        "gugus", "[\"node\"]", "node.utf-8"
     };
     for (int i = 0; i < ARRAY_LEN(get_keys); i++) {
         void *node = json_get_node(js, get_keys[i]);
